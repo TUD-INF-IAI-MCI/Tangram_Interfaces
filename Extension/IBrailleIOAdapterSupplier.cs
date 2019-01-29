26d1bc0a7ce4fd3b6b298ee0b264772ff6434923
Jens B <jens.bornschein@tu-dresden.de>
2019-01-29 15:28:47 +0100

    remove unused and unnecessary functions from one interafce

--- Extension/IBrailleIOAdapterSupplier.cs
+++ Extension/IBrailleIOAdapterSupplier.cs
@@ -28,32 +28,5 @@ namespace tud.mci.tangram.TangramLector.Extension
         /// </returns>
         bool IsMainAdapter();
 
-        /// <summary>
-        /// Determines whether the specified adapter can be used as a monitor for another adapter.
-        /// </summary>
-        /// <param name="monitoringAdapter">The adapter type that can be monitored.</param>
-        /// <returns>
-        /// 	<c>true</c> if the specified adapter is a monitor; otherwise, <c>false</c>.
-        /// </returns>
-        bool IsMonitor(out List<String> monitoringAdapter);
-
-        /// <summary>
-        /// Monitors the adapter.
-        /// </summary>
-        /// <param name="adapter">The adapter to monitor.</param>
-        /// <returns>
-        /// <c>true</c> if the specified adapter can be monitored; otherwise, <c>false</c>.
-        /// </returns>
-        bool StartMonitoringAdapter(IBrailleIOAdapter adapter);
-
-        /// <summary>
-        /// Stops the monitoring of the adapter.
-        /// </summary>
-        /// <param name="adapter">The adapter to monitor.</param>
-        /// <returns>
-        /// <c>true</c> if the specified adapter monitoring was stopped; otherwise, <c>false</c>.
-        /// </returns>
-        bool StopMonitoringAdapter(IBrailleIOAdapter adapter);
-
     }
 }
